d20cd383ae322d6a5b0f812cb4f5167ba2010815
JKrishnaD <jayak5063@gmail.com>
2026-04-22 22:43:25 +0530

    feat: define require types

---
+++ src/states.rs
@@ -0,0 +1,30 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Side {
+    Buy,
+    Sell,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct OrderRequest {
+    pub side: Side,
+    pub price: u64,
+    pub qty: u64,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Eq)]
+pub struct Order {
+    pub id: u64,
+    pub side: Side,
+    pub price: u64,
+    pub qty: u64,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Eq)]
+pub struct Fill {
+    pub maker_order_id: u64,
+    pub taker_order_id: u64,
+    pub price: u64,
+    pub qty: u64,
+}
